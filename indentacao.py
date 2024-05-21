5b63c817050c9875339cb80678212596100eaff8
WendersonFontes <81767673+WendersonFontes@users.noreply.github.com>
2024-05-21 10:38:21 -0300

    Add files via upload

---
+++ indentacao.py
@@ -0,0 +1,13 @@
+import time
+def PensarPor10Segundoso():
+    print('pensando')
+    time.sleep(10)
+    print('Lembrei!')
+
+if 10 > 5:
+    print('10 é maior que 5')
+    if 'hello' == 'hello':
+        print('Hello!')
+
+
+    
