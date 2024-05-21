ab5c015fbb1df6fe984e0c30d4c835770bb018b2
WendersonFontes <81767673+WendersonFontes@users.noreply.github.com>
2024-05-21 14:45:58 -0300

    Add files via upload

---
+++ if_em_python.py
@@ -0,0 +1,14 @@
+devo_levantar = False
+
+esta_muito_frio = True
+
+esta_muito_quente = True
+
+if devo_levantar == True:
+    print('Levantare preparar o café!')
+elif esta_muito_frio == True:
+    print('Ficar quietinho na cama!')
+elif esta_muito_quente == True:
+    print('Aumentar o ventilador')
+else:
+    print('Dormir mais um tanto!')
