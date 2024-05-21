3cd49a6b1f3b4d8e5647766c16ad6b967c983ad1
WendersonFontes <81767673+WendersonFontes@users.noreply.github.com>
2024-05-21 10:30:25 -0300

    Add files via upload

---
+++ variaveis_python.py
@@ -0,0 +1,17 @@
+velocidade_maxima = 240
+print(velocidade_maxima)
+
+frase_de_boas_vindas = 'Olá, seja bem-vindo a Dev aprender!'
+print(frase_de_boas_vindas)
+
+posso_entrar_na_sua_casa = False
+dollar = 5.25
+
+print(type(velocidade_maxima))
+print(type(frase_de_boas_vindas))
+print(type(posso_entrar_na_sua_casa))
+
+velocidade, frase, posso_ou_nao = 100, 'Olá', True
+print(velocidade)
+print(frase)
+print(posso_ou_nao)
